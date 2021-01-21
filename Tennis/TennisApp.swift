0b7814abc463a4dfcdb733b718bfd9d83373f205
Pranav <pranavsuri.433@gmail.com>
2021-01-21 09:39:19 +0800

    initial commit

---
+++ Tennis/TennisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TennisApp.swift
+//  Tennis
+//
+//  Created by Sameer Suri on 21/1/21.
+//
+
+import SwiftUI
+
+@main
+struct TennisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tennis/View/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Tennis
+//
+//  Created by Sameer Suri on 21/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        WelcomeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
